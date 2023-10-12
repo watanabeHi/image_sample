56c43077c0fa77d946b4d934251d27c54f3ba18f
watanabeHi <hide3@mild.ocn.ne.jp>
2023-10-12 13:45:12 +0900

    Add images table function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Image;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,16 +21,14 @@ Route::get('/', function () {
 });
 
 Route::post('/post', function (Request $request) {
-
     $shop = new Shop();
     $shop->text = $request->text;
-    $shop->img_path1 = $request->images[0]->store('images');
-    if ($request->images[1] ?? false) {
-        $shop->img_path2 = $request->images[1]->store('images');
-    }
-    if ($request->images[2] ?? false) {
-        $shop->img_path3 = $request->images[2]->store('images');
-    }
     $shop->save();
+    foreach ($request->images as $input_image) {
+        $image = new Image();
+        $image->img_path = $input_image->store('images');
+        $image->shop_id = $shop->id;
+        $image->save();
+    }
     return redirect('/');
 });
